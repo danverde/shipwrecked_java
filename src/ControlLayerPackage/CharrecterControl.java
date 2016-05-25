3d8d9af0b549bf657d107c27e5acbf519e49934e
Jack <Jack@10.18.150.186>
2016-05-25 13:08:13 -0600

    5/25/2016

---
+++ src/ControlLayerPackage/CharrecterControl.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ControlLayerPackage;
+
+/**
+ *
+ * @author Jack
+ */
+public class CharrecterControl {
+    public double calcDamage(int enemyDefence, int hungerValue, int attackType){
+        
+        //calcDamageInFight(hungervalue, attackType,  enemyDefence);
+        //if (hungerValue &lt; -1 and hungerValue &gt;16);
+        //return -1;
+        //if (enemyDefence &lt; -1 and enemyDefence &gt;6);
+        //return -2;
+        //if (attackType!= 4, 8)
+        //return -3;
+        //damage = hungerValue * 0.3 + attackType – enemyDefence
+        //if damage ( &lt; 0 ) 
+        //return 0;
+        //Else Return damage
+        if (hungerValue < -1 & hungerValue > 16){ 
+            return -1;
+        }
+        if (enemyDefence < -1 & enemyDefence > 6){
+            return -2;
+        }
+        if (attackType != 4 || attackType != 8){
+            return -3;
+        }
+        double damage = hungerValue * 0.3 + attackType- enemyDefence;
+        if (damage > 0){
+            return 0;
+        }
+        return damage;
+    }
+        
+
+}
